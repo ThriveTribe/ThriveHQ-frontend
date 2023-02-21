63b4949d72b6ee096d3c6194874874458a1460ca
mramirez92 <ramirez.92.monica@gmail.com>
2023-02-20 18:41:28 -0600

    todo list mess

---
+++ components/ClockTemp.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const adjustedTime = new Date(time.toLocaleString('en-US', { timeZone: timezone }));
+  let hours = adjustedTime.getHours();
+  let minutes = ('0' + adjustedTime.getMinutes()).slice(-2);
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  // Convert to 12-hour format
+  hours = hours % 12 || 12;
+
+  return (
+    <div id="clock"  className=" font-raleway justify-center text-9xl p-6 ">
+      {`${hours}:${minutes} ${ampm}`}
+    </div>
+  );
+}
--- components/Main.js
+++ components/Main.js
@@ -1,10 +1,42 @@
-import useResource from '../hooks/useResource';
 
-export default function Main(props) {
-  const { resources, deleteResource } = useResource();
+import { lazy, Suspense } from 'react';
+import ToDoList from './ToDoList';
+import TDleft from './TDLEFT';
+import Mantra from './Mantra';
+const Clock = lazy(() => import('../components/ClockTemp'));
 
-    return(
-        <main className='py-6 bg-zinc-900'>
-        </main >
-    )
+export default function Main(){
+  return(
+  
+    <main>
+      <div className='p-50 m-20 flex flex-col flex-grow text-center justify-items-center content-center'>
+      <h1 className= "text-8xl">Hello, *user*</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Clock />
+      </Suspense>
+      <Mantra/>
+      {/* <ToDoList/> */}
+      
+    </div>
+    {/* <ToDoList className/> */}
+    <TDleft className="relative h-32 w-32"/>
+
+    </main>
+
+  )
 }
+
+// ! DO NOT DELETE! RESOURCE/DELETE FUNCTIONALITY
+
+// // import useResource from '../hooks/useResource';
+
+// export default function Main(props) {
+//   // const { resources, deleteResource } = useResource();
+
+//     return(
+//         <main className='py-6 bg-zinc-900'>
+//           <body><h1>hello world </h1></body>
+          
+//         </main >
+//     )
+// }
