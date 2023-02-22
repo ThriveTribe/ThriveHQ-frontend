2b5fe72ad64c479932a5252a58522dff7e977d39
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-02-21 18:17:54 -0800

    got Zen API rendering to component, created placeholder DailyFact component

--- pages/api/zen.js
+++ pages/api/zen.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+
 export default async function ZenHandler(req, res) {
 
     // const [state, setState] = useState({
@@ -9,26 +10,27 @@ export default async function ZenHandler(req, res) {
     // });
 
 
-    let zenData = 'http://127.0.0.1:8000/thrive/zen/'
+    // let zenData = 'http://127.0.0.1:8000/thrive/zen/'
 
-    const options = {
-        method: "GET",
-        headers: {'Content-Type': 'application/json'},
-    };
+    // const options = {
+    //     method: "GET",
+    //     headers: {'Content-Type': 'application/json'},
+    // };
 
-    const response = await fetch(zenData, options);
+    // const response = await fetch(zenData, options);
 
-    const data = await response.json();
-    res.status(200).json(data)
-    console.log(data)
+    // const data = await response.json();
+    // res.status(200).json(data)
+    // console.log(data.q)
 
-    const newState = {
-        author: data.a,
-        quote: data.q,
-        blockQuote: data.h,
-    };
+    // const zenInfo = {
+    //     author: data.a,
+    //     quote: data.q,
+    //     blockQuote: data.h,
+    // };
 
-    setState(prevState => ({ ...prevState, ...newState }));
+    // return zenInfo
+    // setState(prevState => ({ ...prevState, ...newState }));
 
 
 }
