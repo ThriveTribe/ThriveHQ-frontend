44d9cec691a1c178c3b67978e4503447e6d2515a
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-02-24 09:32:08 -0800

    removed API key from weather component, working but not implemented

--- components/Weather.js
+++ components/Weather.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 // const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 // const factUrl = baseUrl + '/thrive/daily_fact/';
+const weatherApiKey = process.env.WEATHER_API_KEY;
 
 const weatherUrl = "https://weather-embed.p.rapidapi.com/forecast?city=seattle&country=US&textColor=white&units=imperial&days=1"
 
@@ -15,7 +16,7 @@ export default function Weather({weatherURL}){
                 const options = {
                     method: "GET",
                     headers: {
-                    'X-RapidAPI-Key': '8917f90294msh668c563d6c1ab7cp194e9ejsnfb3e9e8f1a80',
+                    'X-RapidAPI-Key': weatherApiKey,
 		            'X-RapidAPI-Host': 'weather-embed.p.rapidapi.com'},
                     };
                 const response = await fetch(weatherUrl, options);
